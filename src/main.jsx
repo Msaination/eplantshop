363ecc3f7f9ca5da2daf61bcfeae447eb3cf8a18
Sipho Nzilwane <64207098+Msaination@users.noreply.github.com>
2025-04-02 11:10:36 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
